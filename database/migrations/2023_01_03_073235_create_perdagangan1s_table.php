1bcc13fbafa5612927f325e8b88eea2018897874
MACBOOK <macbookfajri@MACBOOKs-MacBook-Pro.local>
2023-01-03 15:22:09 +0700

    postpertama

---
+++ database/migrations/2023_01_03_073235_create_perdagangan1s_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePerdagangan1sTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('perdagangan1s', function (Blueprint $table) {
+            $table->id();
+            $table->integer('jam');
+            $table->string('ket');
+            $table->integer('jml_Rp');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('perdagangan1s');
+    }
+}
