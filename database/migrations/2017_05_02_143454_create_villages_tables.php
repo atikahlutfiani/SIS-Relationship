e029071acd28235267fc599e4071b36ca41c4d68
MACBOOK <macbookfajri@MACBOOKs-MacBook-Pro.local>
2023-01-13 04:03:13 +0700

    fix API wilayah

---
+++ database/migrations/2017_05_02_143454_create_villages_tables.php
@@ -0,0 +1,38 @@
+<?php
+
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVillagesTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('villages', function(Blueprint $table){
+            $table->id('No');
+            $table->char('id', 10)->index();
+            $table->char('district_id', 7);
+            $table->string('name', 50);
+            $table->foreign('district_id')
+                ->references('id')
+                ->on('districts')
+                ->onUpdate('cascade')->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('villages');
+    }
+}
